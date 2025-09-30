3152defdfd88fbafaef006efcbed45c77cf81d3c
AJ Reyes <albertjasonreyes1996@gmail.com>
2025-09-30 23:28:46 +0800

    setup for theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import theme from './utils/theme.js';
+import {ThemeProvider, createMuiTheme} from "@mui/material/styles";
+import { Button } from '@mui/material';
+export default function App() {
+  return <ThemeProvider theme ={theme}>
+    <Button variant='contained' color='primary'>Default </Button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  </ThemeProvider>
 }
-
-export default App;
